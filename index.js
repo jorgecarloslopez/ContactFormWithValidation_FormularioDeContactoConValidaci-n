3152c5cc4e1b72e739e569d77c0770113266baec
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2024-11-28 13:38:56 -0300

    creating the fields for the form data

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
     event.preventDefault(); //Creating a function for forms
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
   });
